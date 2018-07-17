bbcb3fd997d222165fae4c05cb751d39da077dd7
陈金鑫 <505150704@qq.com>
2018-07-17 16:12:37 +0800

    json

--- server/crawler/api.js
+++ server/crawler/api.js
@@ -1,23 +1,18 @@
-
-import _ from 'lodash'
-import request from 'request-promise'
-import {writeFileSync} from 'fs'
-
 var _allCharacters = []
 
 export const getAllCharacters = async (page = 1) => {
   var res = await request(`https://www.anapioficeandfire.com/api/characters?page=${page}&pageSize=50`)
-  var body = res.body
+  var body = JSON.parse(res.body)
+
   _allCharacters = _.union(_allCharacters, body)
   if (body.length < 50) {
-    writeFileSync('./allCharacters.json', JSON.stringify(_allCharacters), 'utf8')
+    fs.writeFileSync('./allCharacters.json', JSON.stringify(_allCharacters), 'utf8')
+    
     return
   } else {
-    await _.sleep(1000)
+    await sleep(1000)
     console.log(page)
     page++
     getAllCharacters(page)
   }
 }
-
-getAllCharacters()
--- server/crawler/imdb.js
+++ server/crawler/imdb.js
@@ -1,18 +1,31 @@
-import cheerio from 'cheerio'
 import _ from 'lodash'
 import R from 'ramda'
 import rp from 'request-promise'
-import {writeFileSync} from 'fs'
+import cheerio from 'cheerio'
+// request 国外网站的时候使用本地的 VPN
+import Agent from'socks5-http-client/lib/Agent'
+
+const sleep = time => new Promise(resolve => setTimeout(resolve, time))
+const jsonPath = path => resolve(__dirname, '../database/json/', path)
 
 export const getIMDbCharacter = async () => {
+  // imdb 上权游的卡司的页面地址 http://www.imdb.com/title/tt0944947/fullcredits?ref_=tt_cl_sm#cast
   const options = {
     uri: 'http://www.imdb.com/title/tt0944947/fullcredits?ref_=tt_cl_sm#cast',
+    agentClass: Agent,
+    agentOptions: {
+      socksHost: 'localhost',
+      socksPort: 1080 // 本地 VPN 的端口，这里用的 shadowsocks
+    },
     transform: body => cheerio.load(body)
   }
 
-  const $ = await rp(options)
+  var $ = await rp(options)
   var photos = []
-  $('table.cast_list tr.odd, tr.even').each(function() {
+
+  console.log(1)
+  // 拿到所有的 playedBy, nmId, name, chId
+  $('table.cast_list tr.odd, tr.even').each(function () {
     let playedBy = $(this).find('td.itemprop span.itemprop')
     playedBy = playedBy.text()
 
@@ -30,26 +43,80 @@ export const getIMDbCharacter = async () => {
       name,
       chId
     }
+
     photos.push(data)
   })
+
   const fn = R.compose(
     R.map(photo => {
       const reg1 = /\/name\/(.*?)\/\?ref/
-      const reg2 = /\/characters\/(.*?)\?ref/
+      const reg2 = /\/character\/(.*?)\/\?ref/
 
       const match1 = photo.nmId.match(reg1)
       const match2 = photo.chId.match(reg2)
+
       photo.nmId = match1[1]
       photo.chId = match2[1]
 
       return photo
     }),
-    R.filter(photo => photo.playedBy && photo.name && photo.nmId && photo.chId && photo.chId.length > 5)
+    R.filter(photo => photo.playedBy && photo.name && photo.nmId && photo.chId)
   )
 
   photos = fn(photos)
-  writeFileSync('./imdb.json', JSON.stringify(photos, null, 2), 'utf8')
+
   return photos
 }
 
-getIMDbCharacter()
+export const getIMDbProfile = async (url) => {
+  const options = {
+    uri: url,
+    agentClass: Agent,
+    agentOptions: {
+      socksHost: 'localhost',
+      socksPort: 1080 // 本地 VPN 的端口，这里用的 shadowsocks
+    },
+    transform: body => cheerio.load(body)
+  }
+
+  const $ = await rp(options)
+
+  let img = $('a[name="headshot"] img')
+
+  img = img.attr('src')
+
+  if (img) {
+    img = img.split('_V1').shift()
+    img += '_V1.jpg'
+  }
+
+  return img
+}
+
+export const getIMDbImages = async url => {
+  const options = {
+    uri: url,
+    agentClass: Agent,
+    agentOptions: {
+      socksHost: 'localhost',
+      socksPort: 1080 // 本地 VPN 的端口，这里用的 shadowsocks
+    },
+    transform: body => cheerio.load(body)
+  }
+
+  const $ = await rp(options)
+
+  let images = []
+
+  $('div.media_index_thumb_list a img').each(function () {
+    var src = $(this).attr('src')
+
+    if (src) {
+      src = src.split('_V1').shift()
+      src += '_V1.jpg'
+      images.push(src)
+    }
+  })
+
+  return images
+}
